d692e36b67b6a7222c49ba8d0d5668ca534506cb
jorgedasilvabermudez <jorge.dasilvabermudez@cencosud.com>
2022-10-07 10:23:24 -0300

    remove hardcode from home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import Header from '../../components/Header'
 import HeaderContent from '../../components/HeaderContent'
-import menu from '../../assets/menu.svg'
 
 function Home() {
   return (
     <>
       <Header> 
         <HeaderContent>
-        <span className="menuButton"><img src={menu} alt="burgerButton" /></span> 
-          <h1>Titulo</h1>
+        {/* boton menu y titulo */}
         </HeaderContent>   
         <HeaderContent>
-          <input type="search" name="search"/>
-          <button>boton</button>
+        {/* searchbar y boton */}
         </HeaderContent> 
       </Header>          
     </>
