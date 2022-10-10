04f9a9eb584ae2e69ab5bed0ef0c50d10a805db4
maxiCaballo <pablo.caballo@cencosud.com>
2022-10-10 17:05:19 -0300

    Story 7 y 8 Maxi

---
+++ src/components/HeaderSearchInput/SearchInput.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import magnify from "../../assets/magnify.svg";
+import useWindowWidth from "../../hooks/useWindowWidth";
+import "./style.css";
+
+export const SearchInput = ({ setProducts, active, setActive }) => {
+  const [value, setValue] = useState("");
+  const [searchInputProducts, setSearchInputProducts] = useState();
+
+  const windowWidth = useWindowWidth();
+
+  async function handleInput(value) {
+    try {
+      const response = await fetch("http://localhost:4000/api/product");
+      const data = await response.json();
+      setSearchInputProducts(data);
+      setValue(value.toLowerCase());
+      const filterProducts = searchInputProducts.filter(
+        (product) =>
+          product.title.toLowerCase().includes(value) ||
+          product.category.toLowerCase().includes(value) ||
+          product.description.toLowerCase().includes(value) ||
+          product.id === Number(value)
+      );
+      setProducts(filterProducts);
+    } catch (error) {
+      return error;
+    }
+  }
+  function mobileBehavior() {
+    if (windowWidth <= 576) {
+      setActive(!active);
+    }
+  }
+
+  return (
+    <>
+      <div
+        className="inputSearchContainer"
+        style={active ? { width: `${100}vw` } : null}
+        onClick={mobileBehavior}
+      >
+        <button
+          className="btnCloseSearch"
+          onClick={() => setActive(false)}
+          style={
+            windowWidth > 576
+              ? { display: "none" }
+              : active
+              ? { display: "inline" }
+              : { display: "none" }
+          }
+        >
+          X
+        </button>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => handleInput(e.target.value)}
+          placeholder="Buscar productos"
+          onClick={(e) => e.stopPropagation()}
+          style={
+            windowWidth > 576
+              ? { display: "block" }
+              : active
+              ? { display: "block" }
+              : { display: "none" }
+          }
+        />
+        <img src={magnify} alt="magnify glass" className="icon" />
+      </div>
+    </>
+  );
+};
--- src/components/SwitcherTheme/SwitcherTheme.jsx
+++ src/components/SwitcherTheme/SwitcherTheme.jsx
@@ -1,39 +1,54 @@
-import { useContext, useEffect, useState } from 'react';
-import {ThemeContext } from '../../context/ThemeContext'
-import sun from '../../assets/sun-fill.svg'
-import moon from '../../assets/moon-fill.svg'
-import './Switcher.css';
+import { useContext, useEffect, useState } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
+import sun from "../../assets/sun-fill.svg";
+import moon from "../../assets/moon-fill.svg";
+import "./Switcher.css";
 
 const SwitcherTheme = () => {
-
   //document.documentElement.setAttribute("data-theme","dark")
-  const { theme, toggle: switcherFunc }= useContext(ThemeContext)
+  const { theme, toggle: switcherFunc } = useContext(ThemeContext);
   // eslint-disable-next-line
-    const [localTheme, setLocalTheme]= useState("") 
+  const [localTheme, setLocalTheme] = useState("");
 
-    const storageChange=()=>{
-      const currentTheme= (window.localStorage.getItem("theme")==="clearTheme" )? "darkTheme" : "clearTheme" 
-      window.localStorage.setItem("theme",currentTheme)
-      console.log("anda")
+  const storageChange = () => {
+    const currentTheme =
+      window.localStorage.getItem("theme") === "clearTheme"
+        ? "darkTheme"
+        : "clearTheme";
+    window.localStorage.setItem("theme", currentTheme);
+    console.log("anda");
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+    if (localTheme) {
+      setLocalTheme(localTheme);
+    } else {
+      window.localStorage.setItem("theme", "clearTheme");
     }
-
-  useEffect(()=>{
-    const localTheme = window.localStorage.getItem("theme")
-    if (localTheme){setLocalTheme(localTheme)}else{ window.localStorage.setItem("theme","clearTheme")
-  }
-    
-  },[])
+  }, []);
 
   return (
-  <div className='switcherContainer'>    
-    <span className={`iconTheme ${theme}`}><img src={sun} alt="sun logo"/></span>
-    <label className="switcher">
-        <input type="checkbox" defaultChecked={window.localStorage.getItem("theme")==="darkTheme"} onChange={()=>{switcherFunc(); storageChange()} }/>
+    <div className="switcherContainer">
+      <span className={`iconTheme ${theme}`}>
+        <img src={sun} alt="sun logo" className="icon" />
+      </span>
+      <label className="switcher">
+        <input
+          type="checkbox"
+          defaultChecked={window.localStorage.getItem("theme") === "darkTheme"}
+          onChange={() => {
+            switcherFunc();
+            storageChange();
+          }}
+        />
         <span className="slider round"></span>
-    </label>
-    <span className={`iconTheme ${theme}`}><img src={moon} alt="moon logo"/></span> 
-  </div>
-  )
-}
+      </label>
+      <span className={`iconTheme ${theme}`}>
+        <img src={moon} alt="moon logo" className="icon" />
+      </span>
+    </div>
+  );
+};
 
-export default SwitcherTheme
+export default SwitcherTheme;
